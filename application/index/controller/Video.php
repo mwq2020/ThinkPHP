9ca68e96ad239eae715191621b47f7d1d412afe5
mwq2020 <mwq2020@163.com>
2017-11-06 23:27:44 +0800

    fix

---
+++ application/index/controller/Video.php
@@ -0,0 +1,20 @@
+<?php
+namespace app\index\controller;
+
+class Video extends \think\Controller
+{
+    public function index()
+    {
+        return $this->fetch();
+    }
+
+    public function cate()
+    {
+        return $this->fetch();
+    }
+
+    public function info()
+    {
+        return $this->fetch();
+    }
+}
