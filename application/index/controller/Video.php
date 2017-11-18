785860d0312378236b8e799d571a8f4916ab8a07
mwq2020 <mwq2020@163.com>
2017-11-19 00:04:32 +0800

    fix

--- application/index/controller/Video.php
+++ application/index/controller/Video.php
@@ -58,4 +58,53 @@ class Video extends \think\Controller
 
         return $this->fetch('info',$view_data);
     }
+
+    public function video_list()
+    {
+        $cate_id = isset($_REQUEST['cat_id']) && $_REQUEST['cat_id'] > 0 ? intval($_REQUEST['cat_id']) : 1;
+        $sort = isset($_REQUEST['sort']) && in_array($_REQUEST['sort'],array('new','view_num')) ? trim($_REQUEST['sort']) : 'new';
+        $db = Db::table('video_list')->where(['cat_id' => $cate_id]);
+        if($sort == 'new'){
+            $db->order('add_time desc');
+        } else {
+            $db->order('view_num desc');
+        }
+
+        $video_list = $db->paginate(12);
+
+        $page = $video_list->render();
+
+        $view_data = [];
+        $view_data['video_list'] = $video_list;
+        $view_data['page']      = $page;
+        $view_data['total']      = $video_list->total;
+
+        return $this->fetch('video_list',$view_data);
+    }
+
+    /**
+     * 视频详情页面
+     * @return mixed
+     */
+    public function video_info()
+    {
+        $video_info = Db::table('video_list')->where(['id'=>$_REQUEST['id']])->find();
+        if($video_info){
+            $view_num = $video_info['view_num'] + 1;
+            Db::table('video_list')->where(['id'=>$video_info['id']])->update(['view_num'=>$view_num]);
+        }
+
+        $cate_info = Db::table('category')->where(['id'=>$video_info['cat_id']])->find();
+
+        $view_data = [];
+        $view_data['video_info']      = $video_info;
+        $view_data['cate_info']      = $cate_info;
+
+//        echo "<pre>";
+//        print_r($video_info);
+//        exit;
+
+        return $this->fetch('video_info',$view_data);
+    }
+
 }
